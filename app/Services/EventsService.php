84d8b2aec47ef046f90f091fae6c074812d8c6fc
Ranieli <ranielisilveira@hotmail.com>
2023-06-06 07:07:33 -0300

    feat: Creating service Events

---
+++ app/Services/EventsService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Events;
+use App\Repositories\EventsRepository;
+
+class EventsService
+{
+    private $eventsRepository;
+
+    public function __construct(
+        EventsRepository $eventsRepository,
+    ) {
+        $this->eventsRepository = $eventsRepository;
+    }
+
+    public function create(array $data): Events
+    {
+        return $this->eventsRepository->create($data);
+    }
+}
